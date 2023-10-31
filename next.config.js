e50a734fbee850d27b5d7cb668edd774d33b5304
Joshua Comeau <joshwcomeau@gmail.com>
2023-10-31 13:55:16 -0400

    Implement “two-pass rendering” solution

--- next.config.js
+++ next.config.js
@@ -1,26 +1,11 @@
 /*
-  This configuration disables React's "Strict Mode" for this project.
+  In the `final-solution` branch, we've moved to the “Two Pass
+  Rendering” strategy. This strategy works just fine in Strict
+  Mode, and so I've flipped the config back to true.
 
-  As we learned in Module 3, Strict Mode helps us find subtle flaws
-  in our code by turning them into glaringly-obvious problems. This
-  is a good thing! So why on earth am I disabling it??
-
-  Here's the deal: in this lesson, we're exploring one of the biggest
-  gotchas with Server Side Rendering: we can't access the `window`
-  object on the server. The most intuitive solutions to this problem
-  involve conditional logic, but that conditional logic often leads
-  to hydration mismatches.
-
-  I want us to focus on this particular SSR gotcha. And it's easier
-  to focus on that problem when Strict Mode isn't surfacing *other*
-  problems. 😅
-
-  In this same lesson, we learn about a “Two Pass Rendering” strategy.
-  This is the strategy I actually recommend using. It's fully
-  compatible with Strict Mode. You can see the code here:
-
-  https://github.com/joshwcomeau/next-13-ssr-gotchas/blob/final-solution/src/components/Counter/Counter.js
+  (This is the default value, and so we don't actually need to set
+  it, but I'm doing it here to be extra explicit.)
 */
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
 };
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,15 +1,25 @@
 'use client';
 import React from 'react';
 
+import Spinner from '../Spinner';
+
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const savedValue =
+      window.localStorage.getItem('saved-count');
+
+    setCount(savedValue ? Number(savedValue) : 0);
+  }, []);
+
+  React.useEffect(() => {
+    if (typeof count === 'number') {
+      window.localStorage.setItem(
+        'saved-count',
+        count
+      );
+    }
   }, [count]);
 
   return (
@@ -17,7 +27,8 @@ function Counter() {
       className="count-btn"
       onClick={() => setCount(count + 1)}
     >
-      Count: {count}
+      Count:{' '}
+      {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
